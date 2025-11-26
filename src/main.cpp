74feeb1d3aa2aa00559c2a2c65a14079fc563145
Rahaman <rahaman@w2k.ihp-ffo.de>
2025-11-26 16:12:20 +0100

    testing change in main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "greeter.h"
-
-int main(int argc, char** argv) {
-    std::string name = (argc > 1) ? argv[1] : "";
-    std::cout << demo::greet(name) << std::endl;
+int main() {
+    Greeter g("Arifur");
+    std::cout << "Hello, " << g.get_name() << "!" << std::endl;
     return 0;
 }
